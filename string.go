50fa27c86cac8e93752ba31d3f56c7870720ae0a
PǝRV <alexander.w.matulionis@gmail.com>
2017-10-06 15:04:34 -0700

    No more '// created!'

--- string.go
+++ string.go
@@ -17,7 +17,6 @@ func MakeString(name string, buf ...int) (chan<- string, <-chan string) {
 	stringDict.Lock()
 	if stringDict.m == nil {
 		stringDict.m = map[string][]*tstring{}
-		println(`map[string][]*tstring // created!`)
 	}
 	S := &tstring{
 		name: name,
