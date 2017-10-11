87c6159015f516ccda3c1c1c69f165eae119ff18
PǝRV <alexander.w.matulionis@gmail.com>
2017-10-11 13:49:50 -0700

    Made all map types

--- var.go
+++ var.go
@@ -19,77 +19,77 @@ var (
 	wsync  sync.Mutex
 
 	wbooll    sync.Mutex
-	wboolm    map[string][]interface{}
+	wboolm    = make(map[string][]interface{})
 	rbooll    sync.RWMutex
-	rboolm    map[string][]rbool
+	rboolm    = make(map[string][]rbool)
 	wstringl  sync.Mutex
-	wstringm  map[string][]interface{}
+	wstringm  = make(map[string][]interface{})
 	rstringl  sync.RWMutex
-	rstringm  map[string][]rstring
+	rstringm  = make(map[string][]rstring)
 	wintl     sync.Mutex
-	wintm     map[string][]interface{}
+	wintm     = make(map[string][]interface{})
 	rintl     sync.RWMutex
-	rintm     map[string][]rint
+	rintm     = make(map[string][]rint)
 	wint8l    sync.Mutex
-	wint8m    map[string][]interface{}
+	wint8m    = make(map[string][]interface{})
 	rint8l    sync.RWMutex
-	rint8m    map[string][]rint8
+	rint8m    = make(map[string][]rint8)
 	wint16l   sync.Mutex
-	wint16m   map[string][]interface{}
+	wint16m   = make(map[string][]interface{})
 	rint16l   sync.RWMutex
-	rint16m   map[string][]rint16
+	rint16m   = make(map[string][]rint16)
 	wint32l   sync.Mutex
-	wint32m   map[string][]interface{}
+	wint32m   = make(map[string][]interface{})
 	rint32l   sync.RWMutex
-	rint32m   map[string][]rint32
+	rint32m   = make(map[string][]rint32)
 	wint64l   sync.Mutex
-	wint64m   map[string][]interface{}
+	wint64m   = make(map[string][]interface{})
 	rint64l   sync.RWMutex
-	rint64m   map[string][]rint64
+	rint64m   = make(map[string][]rint64)
 	wuintl    sync.Mutex
-	wuintm    map[string][]interface{}
+	wuintm    = make(map[string][]interface{})
 	ruintl    sync.RWMutex
-	ruintm    map[string][]ruint
+	ruintm    = make(map[string][]ruint)
 	wuint8l   sync.Mutex
-	wuint8m   map[string][]interface{}
+	wuint8m   = make(map[string][]interface{})
 	ruint8l   sync.RWMutex
-	ruint8m   map[string][]ruint8
+	ruint8m   = make(map[string][]ruint8)
 	wuint16l  sync.Mutex
-	wuint16m  map[string][]interface{}
+	wuint16m  = make(map[string][]interface{})
 	ruint16l  sync.RWMutex
-	ruint16m  map[string][]ruint16
+	ruint16m  = make(map[string][]ruint16)
 	wuint32l  sync.Mutex
-	wuint32m  map[string][]interface{}
+	wuint32m  = make(map[string][]interface{})
 	ruint32l  sync.RWMutex
-	ruint32m  map[string][]ruint32
+	ruint32m  = make(map[string][]ruint32)
 	wuint64l  sync.Mutex
-	wuint64m  map[string][]interface{}
+	wuint64m  = make(map[string][]interface{})
 	ruint64l  sync.RWMutex
-	ruint64m  map[string][]ruint64
+	ruint64m  = make(map[string][]ruint64)
 	wbytel    sync.Mutex
-	wbytem    map[string][]interface{}
+	wbytem    = make(map[string][]interface{})
 	rbytel    sync.RWMutex
-	rbytem    map[string][]rbyte
+	rbytem    = make(map[string][]rbyte)
 	wbytesl   sync.Mutex
-	wbytesm   map[string][]interface{}
+	wbytesm   = make(map[string][]interface{})
 	rbytesl   sync.RWMutex
-	rbytesm   map[string][]rbytes
+	rbytesm   = make(map[string][]rbytes)
 	wrunel    sync.Mutex
-	wrunem    map[string][]interface{}
+	wrunem    = make(map[string][]interface{})
 	rrunel    sync.RWMutex
-	rrunem    map[string][]rrune
+	rrunem    = make(map[string][]rrune)
 	wfloat32l sync.Mutex
-	wfloat32m map[string][]interface{}
+	wfloat32m = make(map[string][]interface{})
 	rfloat32l sync.RWMutex
-	rfloat32m map[string][]rfloat32
+	rfloat32m = make(map[string][]rfloat32)
 	wfloat64l sync.Mutex
-	wfloat64m map[string][]interface{}
+	wfloat64m = make(map[string][]interface{})
 	rfloat64l sync.RWMutex
-	rfloat64m map[string][]rfloat64
+	rfloat64m = make(map[string][]rfloat64)
 	werrorl   sync.Mutex
-	werrorm   map[string][]interface{}
+	werrorm   = make(map[string][]interface{})
 	rerrorl   sync.RWMutex
-	rerrorm   map[string][]rerror
+	rerrorm   = make(map[string][]rerror)
 )
 
 func init() {
